8b8fad8797908a02e6a0855aff1802776f1058fd
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2016-10-30 15:58:06 -0700

    Week 2

---
+++ src/DinnerDecider.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class DinnerDecider {
+public static void main(String[] args) {
+	ArrayList<String> DinnerType = new ArrayList<String>();
+	DinnerType.add("Steak");
+	DinnerType.add("Hamburger");
+	DinnerType.add("Spaghetti");
+	DinnerType.add("Pasta");
+	DinnerType.add("Meatloaves");
+	
+}
+}
---
+++ src/IntroArrayLists.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class IntroArrayLists {
+	public static void main(String[] args) {
+		ArrayList <String> StringList = new ArrayList<String>();
+		StringList.add("Dave");
+		StringList.add("Evan");
+		StringList.add("Alec");
+		for (String s:StringList) {
+			System.out.println(s);
+		}
+	}
+}
