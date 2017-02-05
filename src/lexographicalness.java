28950b0841de47f7fd7a61b42aec4a0045f0ee54
Revanty <league@WorthyMini.attlocal.net>
2017-02-05 15:57:05 -0800

    Week 12

---
+++ src/lexographicalness.java
@@ -0,0 +1,9 @@
+
+public class lexographicalness {
+	public static void main(String[] args) {
+		String app = "apple";
+		String zeb = "zebra";
+		int val = zeb.compareTo(app);
+		System.out.println(val);
+	}
+}
