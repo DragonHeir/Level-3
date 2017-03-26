73fb01c4035a2a9faacd0bb8c98b7d44ec0f60b8
Revanty <league@WTS-iMac-11.attlocal.net>
2017-03-26 15:38:48 -0700

    Week 17

---
+++ src/AnimalHipster.java
@@ -0,0 +1,21 @@
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AnimalHipster {
+
+	public ArrayList<String> findAnimalHipsters(HashMap<String, ArrayList<String>> network, HashMap<String, String> favoriteAnimals){
+		ArrayList<String> AnimalHipsters = new ArrayList<String> ();
+		for (String s : network.keySet()) {
+			String animal = favoriteAnimals.get(s);
+			for (String t : network.keySet()) {
+				if (!animal.equals(t)){
+					AnimalHipsters.add(s);
+				}
+				
+			}
+		}
+		return AnimalHipsters;
+	}
+
+}
