8ed21adc9e55bbda42beba02a6ed5be001e26f2f
Ollie Pai <ollie.pai2003@gmail.com>
2017-05-28 15:55:34 -0700

    week 24

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,11 +1,14 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -18,6 +21,8 @@ public class ToDoList implements ActionListener{
 	JButton save;
 	JButton load;
 	ArrayList<String> currentList = new ArrayList<String>();
+	JLabel list;
+	String labelText = "";
 	
 public static void main(String[] args) {
 	ToDoList t = new ToDoList();
@@ -31,7 +36,9 @@ ToDoList(){
 	remove = new JButton("Remove");
 	save = new JButton("Save");
 	load = new JButton("Load");
+	list = new JLabel();
 	
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setVisible(true);
 	frame.setSize(275, 100);
 	
@@ -40,6 +47,7 @@ ToDoList(){
 	panel.add(remove);
 	panel.add(save);
 	panel.add(load);
+	panel.add(list);
 	
 	add.addActionListener(this);
 	remove.addActionListener(this);
@@ -50,23 +58,38 @@ ToDoList(){
 @Override
 public void actionPerformed(ActionEvent e) {
 	if(e.getSource()==add){
-		String addInput = JOptionPane.showInputDialog("Add To Do Item:");
+		String addInput = JOptionPane.showInputDialog("Add To Do Item:") + ", ";
 		currentList.add(addInput);
-		System.out.println(currentList);
+			labelText = "";
+		for (int i = 0; i < currentList.size(); i++) {
+			labelText += currentList.get(i);
+			list.setText(labelText);
+		}
 	}
 	if(e.getSource()==remove){
 		String removeInput = JOptionPane.showInputDialog("Enter Number of Task to Remove:");
 		int x = Integer.parseInt(removeInput);
 		currentList.remove(x);
-		System.out.println(currentList);
+		labelText = "";
+		for (int i = 0; i < currentList.size(); i++) {
+			labelText += currentList.get(i);
+			list.setText(labelText);
+		}
 	}
 	if(e.getSource()==save){
 		FileWriter fw = null;
 		try {
-			fw = new FileWriter("/Users/league/Desktop/Level-3/src/junk/ToDo.rtf", true);
+			String fullText = "";
+			fw = new FileWriter("/Users/league/Desktop/Level-3/src/test.txt");
+			labelText = "";
 			for (int i = 0; i < currentList.size(); i++) {
-				fw.write(currentList.get(i));
+				fullText += currentList.get(i) + "\n";
+				for (int j = 0; j < currentList.size(); j++) {
+					labelText += currentList.get(j);
+					list.setText(labelText);
+				}
 			}
+			fw.write(fullText);
 			fw.close();
 		} catch (IOException f) {
 			// TODO Auto-generated catch block
@@ -74,7 +97,35 @@ public void actionPerformed(ActionEvent e) {
 		}
 	}
 	if(e.getSource()==load){
-	
+		try {
+			FileReader fr = new FileReader("/Users/league/Desktop/Level-3/src/test.txt");
+			int c = fr.read();
+			String d = "";
+			while(c != -1){
+				if (c == (int)'\n'){
+					currentList.add(d);
+					d = "";
+				}
+				else{
+					d += (char) c;
+				}
+				labelText = "";
+				for (int i = 0; i < currentList.size(); i++) {
+					labelText += currentList.get(i);
+					list.setText(labelText);
+				}
+				c = fr.read();
+				
+			}
+			currentList = null;
+			fr.close();
+			
+		} catch (FileNotFoundException f) {
+			f.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 	
 }
